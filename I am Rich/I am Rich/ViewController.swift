f59a48034909dc39799b3c468553a14cd7ca952d
macOS <macos@berat.local>
2022-11-24 21:49:33 +0300

    Swift ilk projeler

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by macOS on 24.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
